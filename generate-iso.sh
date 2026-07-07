e13735792ec7e2e834133060a2b83c026ac27d9f
Tutoperduto <48185132+glapiy12@users.noreply.github.com>
2026-07-07 04:50:17 +0300

    Update generate-iso.sh

--- generate-iso.sh
+++ generate-iso.sh
@@ -7,21 +7,24 @@ OUTPUT="${2:-./my-steamos.iso}"
 echo "==> Pulling image ${IMAGE}"
 docker pull "${IMAGE}"
 
-echo "==> Exporting image to tar archive"
-docker save "${IMAGE}" -o /tmp/my-image.tar
-
 echo "==> Creating ISO with bootc-image-builder"
 mkdir -p output
+
+# Создаём заглушку для /var/lib/containers/storage, чтобы bootc-image-builder не ругался
+mkdir -p /tmp/containers-storage/overlay
 
 docker run \
   --rm \
   --privileged \
+  -v /var/run/docker.sock:/var/run/docker.sock \
   -v "$(pwd)/output":/output \
-  -v /tmp/my-image.tar:/tmp/my-image.tar:ro \
+  -v /tmp/containers-storage:/var/lib/containers/storage \
   quay.io/centos-bootc/bootc-image-builder:latest \
   --type iso \
-  --source oci-archive:/tmp/my-image.tar
+  --local \
+  "${IMAGE}"
 
+# Ищем готовый ISO
 ISO_PATH=$(find output -name '*.iso' | head -1)
 if [ -z "$ISO_PATH" ]; then
   echo "Error: ISO not found" >&2
