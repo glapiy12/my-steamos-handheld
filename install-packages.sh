62e73d42b78270126118e54e4961be44a759813e
Tutoperduto <48185132+glapiy12@users.noreply.github.com>
2026-07-07 03:44:28 +0300

    Create install-packages.sh

---
+++ install-packages.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+# Добавляем репозитории
+curl -o /etc/yum.repos.d/rpmfusion-free.repo https://download1.rpmfusion.org/free/fedora/rpmfusion-free.repo
+curl -o /etc/yum.repos.d/rpmfusion-nonfree.repo https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree.repo
+curl -o /etc/yum.repos.d/bieszczaders-kernel-cachyos.repo https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos/repo/fedora-44/bieszczaders-kernel-cachyos-fedora-44.repo
+
+# Устанавливаем все пакеты
+dnf install -y \
+  steam \
+  gamescope \
+  gamescope-session \
+  mangohud \
+  gamemode \
+  wine \
+  winetricks \
+  vulkan-tools \
+  mesa-vulkan-drivers.i686 \
+  mesa-libGL.i686 \
+  steam-devices \
+  ffmpeg-libs \
+  gstreamer1-plugins-{bad-*,good-*,base} \
+  gstreamer1-plugin-openh264 \
+  lame \
+  libavcodec-freeworld \
+  vlc \
+  hwdata \
+  pciutils \
+  usbutils \
+  ryzenadj \
+  power-profiles-daemon \
+  kernel-cachyos \
+  kernel-cachyos-headers \
+  kernel-cachyos-modules \
+  kernel-cachyos-modules-extra
+
+# Очистка кеша dnf для уменьшения размера
+dnf clean all
